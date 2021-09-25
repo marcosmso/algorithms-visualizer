02b8ff0bdc9a9d8e37a879e61aef5ccd12f8e9c6
Marcos <marcosdp12@gmail.com>
2021-09-24 22:47:37 -0300

    implements merge sort

--- sorting/app.js
+++ sorting/app.js
@@ -3,14 +3,18 @@ generate_array_btn = document.querySelector("#generate-array-btn")
 sort_btn = document.querySelector("#sort-btn")
 stop_btn = document.querySelector("#stop-btn")
 algorithm_select = document.querySelector("#algorithm-select")
+array_size_input = document.querySelector("#array-size")
+speed_selector_input = document.querySelector("#speed-selector")
+h1_test = document.querySelector("#h1-test")
 
 const MAX = 200
 const MIN = 10
 const HEIGHT_SCALE = 2
-const NUMBER_OF_ELEMENTS = 400
 const TOTAL_VIEW_WIDTH = 90
-const BLOCK_WIDTH = TOTAL_VIEW_WIDTH/NUMBER_OF_ELEMENTS;
-const DELAY = 100
+
+let delay = 300
+let NUMBER_OF_ELEMENTS = 50
+let BLOCK_WIDTH = TOTAL_VIEW_WIDTH/NUMBER_OF_ELEMENTS
 
 const state = {
 	SORTED: "green",
@@ -22,7 +26,8 @@ const state = {
 const algorithms = {
 	"bubble_sort": bubble_sort,
 	"selection_sort": selection_sort,
-	"insertion_sort": insertion_sort
+	"insertion_sort": insertion_sort, 
+	"merge_sort_handler": merge_sort_handler
 }
 
 var stop = false
@@ -41,44 +46,46 @@ createNumbersDOMElements()
 render_array_state()
 
 function generateRandomArray(){
-    array = [];
-    
-    for(let i = 0; i < NUMBER_OF_ELEMENTS; i++){
-        let randomNumber = Math.floor(Math.random() * (MAX - MIN) + MIN);
-        array.push(randomNumber);
-    } 
+	array = [];
+	
+	for(let i = 0; i < NUMBER_OF_ELEMENTS; i++){
+		let randomNumber = Math.floor(Math.random() * (MAX - MIN) + MIN);
+		array.push(randomNumber);
+	} 
 }
 
 function createNumbersDOMElements(withNumberText = false){
 
-    number_elements = array.map((number) => {
-        let number_element = document.createElement("div")
-        number_element.classList = "array-element"
+	number_elements = array.map((number) => {
+		let number_element = document.createElement("div")
+		number_element.classList = "array-element"
 
-        let number_element_label = document.createElement("label")
-        number_element_label.classList = "array-element-label"    
-        
-        if (withNumberText){
-            number_element_label.innerText = number
-        }
+		let number_element_label = document.createElement("label")
+		number_element_label.classList = "array-element-label"    
+		
+		if (withNumberText){
+			number_element_label.innerText = number
+		}
 
-        number_element.style.height = `${number * HEIGHT_SCALE}px`
-        number_element.style.width = `${BLOCK_WIDTH}vw`
+		number_element.style.height = `${number * HEIGHT_SCALE}px`
+		number_element.style.width = `${BLOCK_WIDTH}vw`
 
-        number_element.appendChild(number_element_label)
+		number_element.appendChild(number_element_label)
 
-        return number_element
-    })
+		return number_element
+	})
 }
 
 function render_array_state(){
-    array_container.innerHTML = ""
-    number_elements.forEach((element)=>{
-        array_container.appendChild(element)
-    })
+	array_container.innerHTML = ""
+	number_elements.forEach((element)=>{
+		array_container.appendChild(element)
+	})
 }
 
 generate_array_btn.addEventListener("click", () => {
+	NUMBER_OF_ELEMENTS = Number(array_size_input.value)
+	BLOCK_WIDTH = TOTAL_VIEW_WIDTH/NUMBER_OF_ELEMENTS
 	generateRandomArray()
 	createNumbersDOMElements()
 	render_array_state()
@@ -88,7 +95,7 @@ sort_btn.addEventListener("click", () => {
 	number_elements.forEach((element)=>{
 		element.style.backgroundColor = state.NOT_SORTED
 	})
-    algorithm = algorithm_select.value
+	algorithm = algorithm_select.value
 	algorithms[algorithm]()
 })
 
@@ -96,15 +103,19 @@ stop_btn.addEventListener("click", () => {
 	stop = true
 })
 
-async function bubble_sort(delay = DELAY){
-	sort_btn.disabled =	true
-    generate_array_btn.disabled = true;
-	stop_btn.disabled =	false
-    
-	let n = array.length;
-    let i, j
+speed_selector_input.addEventListener("input", () => {
+	delay = 1000 - Number(speed_selector_input.value)
+})
 
-    for(i = 0; i < n - 1 ; i++){
+async function bubble_sort(){
+	sort_btn.disabled =	true
+	generate_array_btn.disabled = true;
+	stop_btn.disabled =	false
+		
+	let n = array.length;
+	let i, j
+
+	for(i = 0; i < n - 1 ; i++){
 		for (j = 0; j < n - i - 1; j++){
 			if (stop) {
 				generate_array_btn.disabled = false;
@@ -131,24 +142,24 @@ async function bubble_sort(delay = DELAY){
 
 		}
 		number_elements[j].style.backgroundColor = state.SORTED
-    }
+	}
 	number_elements[0].style.backgroundColor = state.SORTED
-    
-    generate_array_btn.disabled = false;
+		
+	generate_array_btn.disabled = false;
 	sort_btn.disabled = false 
 	stop_btn.disabled =	true
 	stop = false
 }
 
-async function selection_sort(delay = DELAY){
+async function selection_sort(){
 	sort_btn.disabled =	true
-    generate_array_btn.disabled = true;
+	generate_array_btn.disabled = true;
 	stop_btn.disabled =	false
-    
+		
 	let n = array.length;
-    let i, j, min_idx
+	let i, j, min_idx
 
-    for(i = 0; i < n - 1 && !stop; i++){
+	for(i = 0; i < n - 1 && !stop; i++){
 		min_idx = i
 		number_elements[min_idx].style.backgroundColor = state.PROCESSING
 
@@ -173,26 +184,26 @@ async function selection_sort(delay = DELAY){
 		number_elements.swap(i, min_idx)
 		number_elements[i].style.backgroundColor = state.SORTED
 		render_array_state()
-    }
+	}
 	number_elements[n-1].style.backgroundColor = state.SORTED
-    
-    generate_array_btn.disabled = false;
+		
+	generate_array_btn.disabled = false;
 	sort_btn.disabled = false 
 	stop_btn.disabled =	true
 	stop = false
 }
 
-async function insertion_sort(delay = DELAY){
+async function insertion_sort(){
 	sort_btn.disabled =	true
-    generate_array_btn.disabled = true;
+	generate_array_btn.disabled = true;
 	stop_btn.disabled =	false
-    
+		
 	let n = array.length;
-    let i, j
+		let i, j
 
 	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
 
-    for(i = 1; i < n && !stop; i++){
+	for(i = 1; i < n && !stop; i++){
 		if (i < n - 1){
 			number_elements[i+1].style.backgroundColor = state.NEXT
 		}
@@ -222,12 +233,102 @@ async function insertion_sort(delay = DELAY){
 		})	
 	
 		render_array_state()
-    }
+	}
 	// number_elements[n-1].style.backgroundColor = SORTED
-    
-    generate_array_btn.disabled = false;
+		
+	generate_array_btn.disabled = false
 	sort_btn.disabled = false 
 	stop_btn.disabled =	true
 	stop = false
 }
 
+async function merge_sort_handler() {
+	sort_btn.disabled =	true
+	generate_array_btn.disabled = true
+	stop_btn.disabled =	false
+	await merge_sort(0, number_elements.length - 1)
+	generate_array_btn.disabled = false
+	sort_btn.disabled = false 
+	stop_btn.disabled =	true
+	stop = false
+}
+
+async function merge_sort(l, r){
+	if(l >= r){
+		return
+	}
+
+	let m = l + parseInt((r - l)/2)
+	await merge_sort(l, m)
+	await merge_sort(m + 1, r)
+	await merge(l, m, r);
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));	
+	render_array_state()
+}
+
+async function merge(l, m, r){
+	let n1 = m - l + 1
+	let n2 = r - m
+
+	let LV = new Array(n1)
+	let RV = new Array(n2)
+	let L = new Array(n1)
+	let R = new Array(n2)
+
+	for (let i = 0; i < n1; i++){
+		LV[i] = number_elements[l + i]
+		L[i] = array[l + i]
+		LV[i].style.backgroundColor = 'pink'
+	}
+
+ 	for (let j = 0; j < n2; j++){
+		RV[j] = number_elements[m + 1 + j]
+		R[j] = array[m + 1 + j]
+		RV[j].style.backgroundColor = 'red'
+	}
+
+	let i = 0
+	let j = 0
+	let k = l
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			number_elements[k] = LV[i];
+			array[k] = L[i]
+			i++
+		}
+		else {
+			number_elements[k] = RV[j];
+			array[k] = R[j]
+			j++
+		}
+		k++
+	}
+
+	while (i < n1) {
+		number_elements[k] = LV[i];
+		array[k] = L[i];
+		i++
+		k++
+	}
+
+	while (j < n2) {
+		number_elements[k] = RV[j];
+		array[k] = R[j];
+		j++
+		k++
+	}
+
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
+	render_array_state()
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
+	
+	number_elements.forEach((element)=>{
+		element.style.backgroundColor = 'blue'
+	})
+
+	render_array_state()
+	await new Promise((resolve) => setTimeout(() => {resolve();}, delay));
+}
